4d51d5ad501b8f32eb22f2dedc5318da4c56e679
Jorgen <ybeliaev@gmail.com>
2021-01-02 14:23:45 +0200

    работа с ассинхронностью

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import thunk from "redux-thunk"
+
 import {render} from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, compose } from 'redux';
-
-import App from './App';
 import { rootReducer } from './redux/rootReducer';
-import * as serviceWorker from './serviceWorker';
 
 const store = createStore(rootReducer, compose(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
