13c350755563a71ec11d26100535d14120c1a6ba
Jorgen <ybeliaev@gmail.com>
2021-01-02 12:29:21 +0200

    add store dev_tools postReducer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
 
 import App from './App';
+import { rootReducer } from './redux/rootReducer';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(rootReducer, compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
 
-render(<App/>, document.getElementById('root'));
+const app = (
+    <Provider store={store}>
+        <App/>
+    </Provider>
+)
+
+render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/redux/postsReducer.js
@@ -0,0 +1,8 @@
+const initialState = {
+    posts: [],
+    fetchPosts: []
+};
+
+export const postsReducer = (state=initialState, action) => {
+    return state
+}
