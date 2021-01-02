acd29201d3153fd40807507f5bc0ff61b52f5b85
Jorgen <ybeliaev@gmail.com>
2021-01-02 14:53:45 +0200

    useDispatch  useSelector

--- src/components/FetchedPosts.js
+++ src/components/FetchedPosts.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchPost } from '../redux/actions'
 
 import Post from './Post'
 
 
 
-export default ({posts}) => {  
-  
-    return <button
-      className="btn btn-primary"      
-    >Загрузить</button>
+export default () => {  
+  const dispatch = useDispatch()
+  const posts = useSelector(state=>state.posts.fetchPosts)
+console.log("fetchPost: ", posts)
+  if(!posts.length){
+    return <button 
+      className="btn btn-primary" 
+      onClick={()=>dispatch(fetchPost())}
+      >Загрузить</button>
+  }
  
   return posts.map(post => <Post post={post} key={post.id} />)
 }
