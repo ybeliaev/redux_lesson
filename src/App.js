3c29d016f739a6b19ed015f908ebc649e5703c29
Jorgen <ybeliaev@gmail.com>
2021-01-02 13:07:36 +0200

    delete post prpp from Posts

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <div className="row">
         <div className="col">
           <h2>Синхронные Посты</h2>
-          <Posts syncPosts={[1,2,3]}/>
+          <Posts />
         </div>
         <div className="col">
           <h2>Асинхронные посты</h2>
