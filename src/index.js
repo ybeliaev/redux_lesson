ad6516e160c67377c16d64523eae7a30598ffefc
Jorgen <ybeliaev@gmail.com>
2021-01-02 14:25:09 +0200

    add applyMiddleware

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import thunk from "redux-thunk"
 
 import {render} from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { rootReducer } from './redux/rootReducer';
 
 const store = createStore(rootReducer, compose(
+    applyMiddleware(
+        thunk
+    ),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
