a7ab8d83509a7336eb4fd76f7e21382470d208dd
Jorgen <ybeliaev@gmail.com>
2021-01-02 13:33:15 +0200

    add action createPost

---
+++ src/redux/actions.js
@@ -0,0 +1,10 @@
+import { CREATE_POST } from "./types";
+
+import {CREATE_POST} from "./types"
+
+export function createPost(post){
+    return{
+        type: CREATE_POST,
+        payload: post
+    }
+}
