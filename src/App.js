bcfdab67016de929b5badde14954e6753dc9bfe7
Jorgen <ybeliaev@gmail.com>
2021-06-09 23:52:20 +0300

    add ant

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { store } from './store'
+
 import { Profile } from './components'
 
 function App() {
     return (
-        <div className="App">
-            <Profile />
-        </div>
+        <Provider store={store}>
+            <div className="App">
+                <Profile />
+            </div>
+        </Provider>
     )
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export const Profile = () => {
+    const { profile } = useSelector((state) => state.profile)
     return (
         <div className="profile">
-            <h1>Profile</h1>
+            {!!profile ? <h1>{profile.name}</h1> : <h1>No DATA..</h1>}
         </div>
     )
 }
