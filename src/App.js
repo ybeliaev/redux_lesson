164cd0c6290f6aed2bedb0c8880784d29e208763
Jorgen <ybeliaev@gmail.com>
2021-06-09 19:12:22 +0300

    lint husky add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-function App({ posts }) {
+import { getPosts as getPostsAction } from './redux/modules/posts'
+
+function App({ posts, getPosts }) {
     console.log({ posts })
     return <div className="container"></div>
 }
 
-export default connect(({ posts }) => ({ posts }))(App)
+export default connect(({ posts }) => ({ posts }), {
+    getPosts: getPostsAction,
+})(App)
