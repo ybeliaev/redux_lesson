c237e5a730e1b939fc8e006d70c89edf026086e9
Jorgen <ybeliaev@gmail.com>
2021-06-09 12:26:38 +0300

    add ant desine

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-    Count:
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <button type="button" className="btn btn-primary">
+            COUNT: <span class="badge bg-secondary">4</span>
+          </button>
+        </div>
+        <div className="col">
+          Column
+        </div>
+      </div>
     </div>
+    
   );
 }
 
